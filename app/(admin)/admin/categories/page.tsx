82ce843bb816b83d43a19f19f4376056f50ae582
Aminur-Rahman-Kanon <kanon754@gmail.com>
2026-02-11 14:42:55 +0100

    databse_migration_complete

--- app/(admin)/admin/categories/page.tsx
+++ app/(admin)/admin/categories/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useEffect, useState } from 'react';
 import { CategoriesType } from '@/app/(admin)/admin/type/categories';
 import CategoryItem from '../component/categoryItem/categoryItem';
